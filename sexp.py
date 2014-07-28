ee0cd255277448c0f51b446206718dea5a13673f
Eric Davis <eric@davising.com>
2014-07-27 17:07:02 -0700

    By default, strip quotes around strings

--- sexp.py
+++ sexp.py
@@ -4,7 +4,21 @@ def load(fname):
     with open(os.path.expanduser(fname)) as fp:
         return parse(fp.read())
 
-def parse(s):
+def parse(s, strip_quotes=True):
+    """
+    Parse a sexp string, returning a nested list structure.
+
+    If `strip_quotes' is True, strings in the sexp will lose their
+    surrounding quotation marks:
+
+    >>> parse('(:name "Eric")')
+    [':name', 'Eric']
+
+    >> parse('(:name "Eric")', False)
+    [':name', '"Eric"']
+
+    By default, `strip_quotes' is True.
+    """
     result, stack = [], []
     idx = 0
 
@@ -40,7 +54,10 @@ def parse(s):
 
         elif c == '"':
             m = re.search('("[^"]+")', s[idx:])
-            result.append(m.group())
+            if strip_quotes:
+                result.append(m.group()[1:-1])
+            else:
+                result.append(m.group())
             idx += len(m.group())
 
         elif re.search('[-\d]', c):
--- sexp_test.py
+++ sexp_test.py
@@ -9,18 +9,18 @@ testcases = [
     ('(1 2 a b c)', [1, 2, 'a', 'b', 'c']),
     ('(a (b c) d (1 2 3))', ['a', ['b', 'c'], 'd', [1, 2, 3]]),
     ('(1 nil 3)', [1, [], 3]),
-    ('(1 "foo" b)', [1, '"foo"', 'b']),
-    ('(:name "eric" :age 25)', [':name', '"eric"', ':age', 25]),
+    ('(1 "foo" b)', [1, 'foo', 'b']),
+    ('(:name "eric" :age 25)', [':name', 'eric', ':age', 25]),
 
     ('(org-data nil (headline (:title (#("Hello World" 0 11 (:parent #0))))))',
-     ['org-data', [], ['headline', [':title', [['"Hello World"', 0, 11, [':parent', '#0']]]]]]),
+     ['org-data', [], ['headline', [':title', [['Hello World', 0, 11, [':parent', '#0']]]]]]),
 
     # http://rosettacode.org/wiki/S-Expressions
     ('''((data "quoted data" 123 4.5)
       (data (!@# (4.5) "(more" "data)")))''',
 
-     [['data', '"quoted data"', 123, 4.5],
-      ['data', ['!@#', [4.5], '"(more"', '"data)"']]]),
+     [['data', 'quoted data', 123, 4.5],
+      ['data', ['!@#', [4.5], '(more', 'data)']]]),
 ]
 
 def test_sexp_exception():
